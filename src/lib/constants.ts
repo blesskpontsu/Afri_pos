db38efe107ecf13e60ac5ed00b7456622461347d
blesskpontsu <blesskpontsu@gmail.com>
2025-11-25 07:24:22 +0000

    fixes

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,7 @@
 export const API_URL =
-  import.meta.env.VITE_API_URL ?? 'https://app.afriluck.com/api/V1'
+  import.meta.env.VITE_API_URL ?? "https://api-agent.afriluck.com/api/V1";
 export const OTP_URL =
-  import.meta.env.VITE_OTP_URL ?? 'https://app.afriluck.com/api/V1'
+  import.meta.env.VITE_OTP_URL ?? "https://sms.afriluck.com:19972/api/v1";
 export const OTP_API_KEY =
-  import.meta.env.VITE_OTP_API_KEY ?? 'https://app.afriluck.com/api/V1'
+  import.meta.env.VITE_OTP_API_KEY ??
+  "af_cr5OYUDWr0sojzkue1YcDbo5ax3VUZSccx2PmMKw";
